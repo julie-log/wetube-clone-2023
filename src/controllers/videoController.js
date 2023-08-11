24cacfeb69a3fcca6cea02848f7a151aec9c7ff1
julie-log <wnfl2828@gmail.com>
2023-08-11 14:46:56 +0900

    #4.7 URL Parameters part One (10:27)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,12 @@
 export const trending = (req, res) => res.send("Home");
 export const search = (req, res) => res.send("Search Video");
 
-export const see = (req, res) => res.send("Watch Video");
-export const edit = (req, res) => res.send("Edit Video");
+export const see = (req, res) => {
+  return res.send(`Watch ${req.params.id} Video`);
+};
+
+export const edit = (req, res) => {
+  return res.send(`Edit ${req.params.id} Video`);
+};
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,9 +3,9 @@ import { edit, remove, logout, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get("/:id", see);
 userRouter.get("/edit", edit);
 userRouter.get("/logout", logout);
 userRouter.get("/remove", remove);
+userRouter.get("/:id", see);
 
 export default userRouter;
