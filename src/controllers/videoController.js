2f5c3f01f815b94027af58624fda7dfb6522f83f
julie-log <wnfl2828@gmail.com>
2023-08-11 15:39:50 +0900

    #5.3 Extending Templates (10:16)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,7 @@
 export const trending = (req, res) => res.render("home");
 export const search = (req, res) => res.send("Search Video");
-
 export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 
-export const edit = (req, res) => {
-  return res.send(`Edit ${req.params.id} Video`);
-};
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
