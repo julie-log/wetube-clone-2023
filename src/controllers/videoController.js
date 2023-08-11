99c469540a2a5caf7967ccd1a481dc0ab1ae063e
julie-log <wnfl2828@gmail.com>
2023-08-11 20:21:51 +0900

    #5.9 Mixins (12:44)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,35 @@ const fakeUser = {
 };
 
 export const trending = (req, res) => {
-  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const videos = [
+    {
+      title: "Seventeen - Super",
+      rating: 5,
+      comments: 2000,
+      createdAt: "2 months ago",
+      views: "2.5M",
+      id: 1,
+      src: "https://www.youtube.com/embed/-GQg25oP0S4",
+    },
+    {
+      title: "부석순 (SEVENTEEN) - 파이팅 해야지 (Feat. 이영지) Official MV",
+      rating: 5,
+      comments: 3000,
+      createdAt: "6 months ago",
+      views: "3.5M",
+      id: 2,
+      src: "https://www.youtube.com/embed/xj5XzCBCwsc",
+    },
+    {
+      title: "SEVENTEEN- F*ck My Life Official MV",
+      rating: 4.5,
+      comments: 1000,
+      createdAt: "2 months ago",
+      views: "1.5M",
+      id: 3,
+      src: "https://www.youtube.com/embed/9kpuOoIh3wU",
+    },
+  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 
