4e53b154708141a494b96289eb3260732a927fe2
julie-log <wnfl2828@gmail.com>
2023-08-11 15:55:56 +0900

    #5.5 Recap (10:35)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,9 @@
 export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("Search Video");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const see = (req, res) =>
+  res.render("watch", { pageTitle: `Watch Video ${req.params.id}` });
+export const edit = (req, res) =>
+  res.render("edit", { pageTitle: `Edit Video ${req.params.id}` });
 
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
