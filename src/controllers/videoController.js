02386ea7b2af448c7781ca0b858bc1ae025c7857
julie-log <wnfl2828@gmail.com>
2023-08-12 09:42:57 +0900

    #6.1 Array Database part Two (10:11)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,7 +36,7 @@ export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const see = (req, res) => {
+export const watch = (req, res) => {
   const id = req.params.id;
   const video = videos[id - 1];
   return res.render("watch", {
@@ -45,8 +45,12 @@ export const see = (req, res) => {
   });
 };
 
-export const edit = (req, res) =>
-  res.render("edit", { pageTitle: `Edit Video ${req.params.id}` });
+export const edit = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Edit Video ${video.title}`, video });
+};
+
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
