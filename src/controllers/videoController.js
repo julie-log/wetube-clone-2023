bad2f623f9fba516f0d94aaf5de6637e1b8873cc
julie-log <wnfl2828@gmail.com>
2023-08-12 09:25:26 +0900

    #6.0 Array Database part One (11:29)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,45 +2,51 @@ const fakeUser = {
   username: "vernon",
   loggedIn: true,
 };
+let videos = [
+  {
+    title: "Seventeen - Super",
+    rating: 5,
+    comments: 2000,
+    createdAt: "2 months ago",
+    views: "2.5M",
+    id: 1,
+    src: "https://www.youtube.com/embed/-GQg25oP0S4",
+  },
+  {
+    title: "부석순 (SEVENTEEN) - 파이팅 해야지 (Feat. 이영지) Official MV",
+    rating: 5,
+    comments: 3000,
+    createdAt: "6 months ago",
+    views: "3.5M",
+    id: 2,
+    src: "https://www.youtube.com/embed/xj5XzCBCwsc",
+  },
+  {
+    title: "SEVENTEEN- F*ck My Life Official MV",
+    rating: 4.5,
+    comments: 1000,
+    createdAt: "2 months ago",
+    views: "1.5M",
+    id: 3,
+    src: "https://www.youtube.com/embed/9kpuOoIh3wU",
+  },
+];
 
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Seventeen - Super",
-      rating: 5,
-      comments: 2000,
-      createdAt: "2 months ago",
-      views: "2.5M",
-      id: 1,
-      src: "https://www.youtube.com/embed/-GQg25oP0S4",
-    },
-    {
-      title: "부석순 (SEVENTEEN) - 파이팅 해야지 (Feat. 이영지) Official MV",
-      rating: 5,
-      comments: 3000,
-      createdAt: "6 months ago",
-      views: "3.5M",
-      id: 2,
-      src: "https://www.youtube.com/embed/xj5XzCBCwsc",
-    },
-    {
-      title: "SEVENTEEN- F*ck My Life Official MV",
-      rating: 4.5,
-      comments: 1000,
-      createdAt: "2 months ago",
-      views: "1.5M",
-      id: 3,
-      src: "https://www.youtube.com/embed/9kpuOoIh3wU",
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const search = (req, res) => res.send("Search Video");
-export const see = (req, res) =>
-  res.render("watch", { pageTitle: `Watch Video ${req.params.id}` });
+export const see = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id - 1];
+  return res.render("watch", {
+    pageTitle: `Watch Video ${video.title}`,
+    video,
+  });
+};
+
 export const edit = (req, res) =>
   res.render("edit", { pageTitle: `Edit Video ${req.params.id}` });
-
+export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
