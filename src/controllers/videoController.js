fac2debdca589933f343adbbfaed893669e834c3
julie-log <wnfl2828@gmail.com>
2023-08-13 15:25:33 +0900

    #6.21 Edit Video part Two (09:08)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -63,9 +63,12 @@ export const getEdit = async (req, res) => {
   return res.render("edit", { pageTitle: `Edit : ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found..." });
+  }
   return res.redirect(`/videos/${id}`);
 };
 
