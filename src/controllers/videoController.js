2dc4d6a6a05b6abf4a4112f62e6bb70f9e22e378
julie-log <wnfl2828@gmail.com>
2023-08-14 10:52:24 +0900

    #6.27 Search part Two (12:12)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -104,9 +104,15 @@ export const deleteVideo = async (req, res) => {
   return res.redirect("/");
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const { keyword } = req.query;
+  let videos = [];
   if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}`, "i"),
+      },
+    });
   }
-  return res.render("search", { pageTitle: `Search Video` });
+  return res.render("search", { pageTitle: `Search Video`, videos });
 };
