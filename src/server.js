bd8d358c24700a3b09b3d9b653b1f805393d5e8b
julie-log <wnfl2828@gmail.com>
2023-08-15 12:06:47 +0900

    #7.8 Sessions and Cookies part Two (12:48)

--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   session({
     secret: "Hello!",
@@ -20,12 +21,18 @@ app.use(
     saveUninitialized: true,
   })
 );
+
 app.use((req, res, next) => {
   req.sessionStore.all((error, sessions) => {
     console.log(sessions);
     next();
   });
 });
+
+app.get("/add-one", (req, res, next) => {
+  return res.send(`${req.session.id}`);
+});
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
