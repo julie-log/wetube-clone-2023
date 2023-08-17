26d318df2cc1d48d8a35709b462ae39047553dd0
julie-log <wnfl2828@gmail.com>
2023-08-17 09:43:07 +0900

    #7.21 Github Login part Six (13:54)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -108,11 +108,29 @@ export const finishGithubLogin = async (req, res) => {
       })
     ).json();
     console.log(emailData);
-    const email = emailData.find(
+    const emailObj = emailData.find(
       (email) => email.primary === true && email.verified === true
     );
-    if (!email) {
+    if (!emailObj) {
       res.redirect("/login");
+    }
+    const existingUser = await User.findOne({ email: emailObj.email });
+    if (existingUser) {
+      req.session.loggedIn = true;
+      req.session.user = existingUser;
+      return res.redirect("/");
+    } else {
+      const user = await User.create({
+        name: userData.name ? userData.name : "Unknown",
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+      req.session.loggedIn = true;
+      req.session.user = user;
+      return res.redirect("/");
     }
   } else {
     return res.redirect("/login");
