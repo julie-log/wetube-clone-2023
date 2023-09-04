86df9d1ba5875631008a8ce2a2868d8cdf267055
julie-log <wnfl2828@gmail.com>
2023-09-04 21:14:19 +0900

    change

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -40,7 +40,6 @@ export const home = async (req, res) => {
   const videos = await Video.find({})
     .sort({ createdAt: "desc" })
     .populate("owner");
-  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
 
